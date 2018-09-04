b696318aab2f2fff005764dee5d972a658c7c710
Justin Ndwiga <ndwigajustin@gmail.com>
2018-09-04 14:38:22 +0300

    [feature #160237610] Order class model

---
+++ app/v1/models/order.py
@@ -0,0 +1,36 @@
+class Order:
+    """constructor and methods for the Order model"""
+    def __init__(self):
+        self.orders = {}
+    
+    def create_order(self, owner, items, price, servings, status='pending'):
+        """Adds a new order to the orders dictionary"""
+        new_order = {'order_id': len(self.orders) + 1,
+                    'owner': owner,
+                    'items': items,
+                    'price': price,
+                    'servings': servings,
+                    'total': price * servings,
+                    'status': status}
+        self.orders[owner] = new_order
+        return self.orders
+
+    def find_order_by_id(self, order_id):
+        """Update the staus of an order"""
+        if self.orders:
+            for order in self.orders.values():
+                if order.get('order_id') == order_id:
+                    return order
+    
+    def update_order(self, order_id, status):
+        order = self.find_order_by_id(order_id)
+        if order:
+            order['status'] == status
+            return order
+    
+    def delete_order(self,order_id):
+        order = self.find_order_by_id(order_id)
+        if order:
+            if order['status'] == 'decline':
+                del self.orders[order_id]
+                return self.orders
