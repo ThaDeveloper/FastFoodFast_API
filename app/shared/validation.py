7f2cedb2389a2084215143233d1f07d17b1f6355
Justin Ndwiga <ndwigajustin@gmail.com>
2018-09-07 11:21:16 +0300

    Fix - remove cognitive complexity

--- app/shared/validation.py
+++ app/shared/validation.py
@@ -10,23 +10,21 @@ def validate_user(data):
     if not isinstance(data['username'], str):
         return jsonify({"Message":
                         "Wrong username format: Can only be a string"}), 400
-    if len(
-            data['username'].strip()) == 0 or len(
-            data['password'].strip()) == 0:
+    elif len(data['username'].strip()) == 0 or\
+            len(data['password'].strip()) == 0 or\
+            len(data['first_name'].strip()) == 0 or\
+            len(data['last_name'].strip()) == 0:
         return jsonify({'Message':
-                        "Username and Password is required"}), 400
-    for x in data['username']:
-        if x.isspace():
-            return jsonify({"Message":
-                            "Username can't contain spaces"}), 400
-    if len(data['username'].strip()) < 3:
+                        "All fields are required"}), 400
+    elif len(data['username'].strip()) < 3:
         return jsonify({"Message":
                         "Username must be 3 characters and above"}), 400
-    if not(validate_email(data["email"])):
-            return jsonify({'Message':
-                        'Enter a valid email'}), 400
-    if len(
-            data['first_name'].strip()) == 0 or len(
-            data['last_name'].strip()) == 0:
+
+    elif not(validate_email(data["email"])):
         return jsonify({'Message':
-                        "First Name and Last Name is required"}), 400
+                        'Enter a valid email'}), 400
+    else:
+        for x in data['username']:
+            if x.isspace():
+                return jsonify({"Message":
+                                "Username can't contain spaces"}), 400
