9f7bfa57a5e9e183a914f315696839d13cd261e6
Justin Ndwiga <ndwigajustin@gmail.com>
2018-09-23 16:03:52 +0300

    [#160708921] Update tests to run with version2

--- app/v2/database.py
+++ app/v2/database.py
@@ -5,12 +5,13 @@ from psycopg2.extras import RealDictCursor
 
 class Database:
     """connecting to db, creating and droping database tables"""
+
     def __init__(self):
         """Initialize by setting up and connecting to database"""
         if os.getenv('FLASK_ENV') == 'development':
             self.database = os.getenv("DEV_DATABASE")
         elif os.getenv('FLASK_ENV') == 'testing':
-            self.database= os.getenv("TEST_DATABASE")
+            self.database = os.getenv("TEST_DATABASE")
         self.user = os.getenv('USER')
         self.password = os.getenv('PASSWORD')
         self.host = os.getenv('HOST')
@@ -20,10 +21,10 @@ class Database:
                 user=self.user,
                 host=self.host,
                 password=self.password)
-        except:
+        except BaseException:
             print("Can't connet to database")
 
-    @staticmethod     
+    @staticmethod
     def tables():
         """List of tables to be created"""
         queries = [
@@ -37,7 +38,7 @@ class Database:
                     admin bool\
                     )',
 
-            
+
             'CREATE TABLE IF NOT EXISTS categories(\
                     cat_id SERIAL PRIMARY KEY,\
                     name VARCHAR(50))\
@@ -73,18 +74,17 @@ class Database:
                     )'
         ]
         return queries
-    
-    
+
     def cursor(self):
         """cursor method for executing queries
         RealDictCursor - A cursor that uses a real python dict as the base type for rows."""
         cur = self.connection.cursor(cursor_factory=RealDictCursor)
         return cur
-    
+
     def commit(self):
         """for saving changes permanently to db"""
         self.connection.commit()
-    
+
     def create_tables(self):
         """create all tables else return executuon error"""
         cur = self.cursor()
@@ -93,6 +93,7 @@ class Database:
                 cur.execute(table)
                 self.commit()
             print('All tables created sucessfully')
+            cur.close()
         except (Exception, psycopg2.DatabaseError) as error:
             print(error)
 
@@ -100,17 +101,18 @@ class Database:
         """drop tables esle return exection error"""
         cur = self.cursor()
         table_drops = ["DROP TABLE IF EXISTS users CASCADE",
-                        "DROP TABLE IF EXISTS categories CASCADE",
-                        "DROP TABLE IF EXISTS menu",
-                        "DROP TABLE IF EXISTS orders",
-                        "DROP TABLE IF EXISTS tokens",
-                        "DROP TABLE IF EXISTS blacklist"
-                     ]
-      
+                       "DROP TABLE IF EXISTS categories CASCADE",
+                       "DROP TABLE IF EXISTS menu",
+                       "DROP TABLE IF EXISTS orders",
+                       "DROP TABLE IF EXISTS tokens",
+                       "DROP TABLE IF EXISTS blacklist"
+                       ]
+
         try:
             for table in table_drops:
                 cur.execute(table)
                 self.connection.commit()
             print("All tables dropped successfully")
+            cur.close()
         except (Exception, psycopg2.DatabaseError) as error:
             print(error)
