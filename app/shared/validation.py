f47ef4c2c4374ddf8d96b9d42e1e34fe0a091a44
Justin Ndwiga <ndwigajustin@gmail.com>
2018-09-23 12:31:21 +0300

    [#160621108] use regex for email, freeze reqs

--- app/shared/validation.py
+++ app/shared/validation.py
@@ -1,5 +1,4 @@
 from flask import jsonify
-from validate_email import validate_email
 import re
 
 
@@ -13,16 +12,16 @@ def validate_user(data):
         return jsonify({"Message":
                         "Wrong username format: Can only be\
                          a string"}), 400
+    elif not  re.match("^[a-zA-Z_.-]{3,15}$", username):
+        return jsonify({"Message":
+                        "Username must be a commbination of 3-15 letters plus special"
+                        " chars (a-zA-Z_.-)"}), 400
     elif not  re.match("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])"
     "(?=.*[@#$%^&+=*]).{6,}$",data['password']):
         return jsonify({"Message":
                         "Password must be 6-20 chars long:"
                         "Must contain capital,number and special char"}), 400
-    elif not  re.match("^[a-zA-Z_.-]{3,15}$", username):
-        return jsonify({"Message":
-                        "Username must be a commbination of 3-15 letters plus special"
-                        " chars (a-zA-Z_.-)"}), 400
-    elif not(validate_email(data["email"])):
+    elif not re.match("^[\w.-]+@([\w-]+)\.+\w{2,}$",data['email']):
         return jsonify({'Message':
                         'Enter a valid email'}), 400
     elif not  re.match("^[a-zA-Z]{2,15}$", data['first_name'])\
